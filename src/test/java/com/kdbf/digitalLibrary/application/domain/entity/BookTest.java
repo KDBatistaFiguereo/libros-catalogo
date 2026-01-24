34b6e4eba75cafc6df3652153618e8300bfb2d25
bibi <kevindaniel707070@gmail.com>
2026-01-24 19:37:58 -0400

    test(validation): Verify constraints for  null and empty book titles.

--- src/test/java/com/kdbf/digitalLibrary/application/domain/entity/BookTest.java
+++ src/test/java/com/kdbf/digitalLibrary/application/domain/entity/BookTest.java
@@ -2,6 +2,7 @@ package com.kdbf.digitalLibrary.application.domain.entity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -22,4 +23,17 @@ public class BookTest {
     assertEquals(downloads, myBook.getDownloads());
   }
 
+  @Test
+  public void shouldNotCreateBookWithEmptyTitle() {
+    NoTitleException exception = assertThrows(NoTitleException.class, () -> {
+      Book myBook = new Book("", "Anonymous", null, null);
+    })
+  }
+
+  @Test
+  public void shouldNotCreateBookWithNullTitle(){
+    NoTitleException exception = assertThrows(NoTitleException.class, () -> {
+      Book myBook = new Book(null, "Anonymous", null, null);
+    })
+  }
 }
