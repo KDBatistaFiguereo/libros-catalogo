2c79311d75e1fa3df36c35a91e22db9dd03e81bb
bibi <kevindaniel707070@gmail.com>
2026-01-24 20:02:29 -0400

    feat(domain): Implement domain constraints for book titles

--- src/main/java/com/kdbf/digitalLibrary/application/domain/entity/Book.java
+++ src/main/java/com/kdbf/digitalLibrary/application/domain/entity/Book.java
@@ -2,6 +2,8 @@ package com.kdbf.digitalLibrary.application.domain.entity;
 
 import java.util.UUID;
 
+import com.kdbf.digitalLibrary.application.domain.exception.NoTitleException;
+
 import lombok.Getter;
 
 @Getter
@@ -14,6 +16,15 @@ public class Book {
   private int downloads;
 
   public Book(String title, String author, String language, int downloads) {
+
+    if (title == null) {
+      throw new NoTitleException("A book's title cant be null.");
+    }
+    title = title.trim();
+    if (title.isEmpty()) {
+      throw new NoTitleException("A book's title cant be empty.");
+    }
+
     this.publicid = UUID.randomUUID();
     this.title = title;
     this.author = author;
