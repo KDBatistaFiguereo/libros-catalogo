e801b991eacbd611547b38e3b113cf389ff63eb8
bibi <kevindaniel707070@gmail.com>
2026-01-27 22:23:44 -0400

    refactor(arch): Migrate mapping logic into its own subpackage

--- src/main/java/com/kdbf/digitalLibrary/adapters/out/api/gutenberg/mapper/GutenbergMapper.java
+++ src/main/java/com/kdbf/digitalLibrary/adapters/out/api/gutenberg/mapper/GutenbergMapper.java
@@ -1,7 +1,9 @@
-package com.kdbf.digitalLibrary.adapters.out.api.gutenberg;
+package com.kdbf.digitalLibrary.adapters.out.api.gutenberg.mapper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kdbf.digitalLibrary.adapters.out.api.gutenberg.dto.GutenbergBookDto;
 import com.kdbf.digitalLibrary.adapters.out.api.gutenberg.dto.GutenbergResponseDto;
+import com.kdbf.digitalLibrary.application.domain.model.entity.Book;
 
 public class GutenbergMapper {
   private final ObjectMapper objectMapper;
@@ -13,6 +15,10 @@ public class GutenbergMapper {
   public GutenbergResponseDto toDto(String jsonResponse) throws Exception {
     GutenbergResponseDto response = objectMapper.readValue(jsonResponse, GutenbergResponseDto.class);
     return response;
+  }
+
+  public Book toDomain(GutenbergBookDto bookDto) {
+    return null;
   }
 
 }
