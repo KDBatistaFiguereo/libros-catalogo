22838718a6f63fcc6abd623bb9b55b26748d2c37
bibi <kevindaniel707070@gmail.com>
2026-01-27 22:25:48 -0400

    feat(mapper): Implement mapper for books

---
+++ src/main/java/com/kdbf/digitalLibrary/adapters/out/api/gutenberg/mapper/GutenbergBookMapper.java
@@ -0,0 +1,31 @@
+package com.kdbf.digitalLibrary.adapters.out.api.gutenberg.mapper;
+
+import java.util.List;
+import java.util.Set;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import com.kdbf.digitalLibrary.adapters.out.api.gutenberg.dto.GutenbergAuthorDto;
+import com.kdbf.digitalLibrary.adapters.out.api.gutenberg.dto.GutenbergBookDto;
+import com.kdbf.digitalLibrary.application.domain.model.entity.Author;
+import com.kdbf.digitalLibrary.application.domain.model.entity.Book;
+
+@Mapper
+public abstract class GutenbergBookMapper {
+  @Mapping(source = "title", target = "title")
+  @Mapping(source = "authors", target = "author", qualifiedByName = "getFirstAuthor")
+  @Mapping(source = "languages", target = "language", qualifiedByName = "getFirstLanguage")
+  @Mapping(source = "downloadCount", target = "downloads")
+  public abstract Book toDomain(GutenbergBookDto dto);
+
+  @Named("getFirstAuthor")
+  protected Author getFirstAuthor(Set<GutenbergAuthorDto> authors) {
+    return new Author(authors.iterator().next().name());
+  }
+
+  @Named("getFirstLanguage")
+  protected String getFirstLanguage(List<String> languages) {
+    return languages.get(0);
+  }
+}
