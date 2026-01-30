2103c5ae94af418b22f359a75330b70ee20c3af1
bibi <kevindaniel707070@gmail.com>
2026-01-30 19:44:21 -0400

    test(common): verify duplicate logic in BookSearchHistory class

---
+++ src/test/java/com/kdbf/digitalLibrary/common/session/BookSearchHistoryTest.java
@@ -0,0 +1,54 @@
+package com.kdbf.digitalLibrary.common.session;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.kdbf.digitalLibrary.application.domain.model.entity.Author;
+import com.kdbf.digitalLibrary.application.domain.model.entity.Book;
+
+public class BookSearchHistoryTest {
+
+  @Test
+  public void shouldAddBooks() {
+
+    List<Book> myBooks = List.of(
+        new Book("Crime and punishment",
+            new Author("Fyodor Dostoyevsky"),
+            "en",
+            10),
+        new Book("Don Quixote",
+            new Author("Cervantes"),
+            "en",
+            15));
+    BookSearchHistory searchHistory = new BookSearchHistory();
+
+    searchHistory.addBooks(myBooks);
+
+    assertEquals(2, searchHistory.getBookHistory().size());
+    assertEquals("Crime and punishment", searchHistory.getBookHistory().get(0).getTitle());
+    assertEquals("Don Quixote", searchHistory.getBookHistory().get(1).getTitle());
+  }
+
+  @Test
+  public void shouldNotAddDuplicates() {
+    List<Book> myBooks = List.of(
+        new Book("Crime and punishment",
+            new Author("Fyodor Dostoyevsky"),
+            "en",
+            10),
+        new Book("Don Quixote",
+            new Author("Cervantes"),
+            "en",
+            15));
+    BookSearchHistory searchHistory = new BookSearchHistory();
+
+    searchHistory.addBooks(myBooks);
+    searchHistory.addBooks(myBooks); // add the books again
+                                     //
+    assertEquals(2, searchHistory.getBookHistory().size());
+  }
+
+}
