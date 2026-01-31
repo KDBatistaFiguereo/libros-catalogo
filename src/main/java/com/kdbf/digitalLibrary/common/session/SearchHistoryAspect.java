6fbcbc51376da20e23a30ce28e0344d2690bc09d
bibi <kevindaniel707070@gmail.com>
2026-01-30 20:01:23 -0400

    feat(infra): implement aspect to add books to history

---
+++ src/main/java/com/kdbf/digitalLibrary/common/session/SearchHistoryAspect.java
@@ -0,0 +1,28 @@
+package com.kdbf.digitalLibrary.common.session;
+
+import java.util.List;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import com.kdbf.digitalLibrary.application.domain.model.entity.Book;
+
+@Aspect
+@Component
+public class SearchHistoryAspect {
+
+  private final BookSearchHistory searchHistory;
+
+  public SearchHistoryAspect(BookSearchHistory searchHistory) {
+    this.searchHistory = searchHistory;
+  }
+
+  @AfterReturning(pointcut = "execution(List<Book> com.kdbf.digitalLibrary.application.service." +
+      "FindBooksByTitleService.findBooksByTitle(..))", returning = "books")
+  private void registerHistory(List<Book> books) {
+    searchHistory.addBooks(books);
+
+  }
+
+}
