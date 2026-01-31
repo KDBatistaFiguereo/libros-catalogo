7cc7212b99129ab1e5cd1f78c5897100ef41dcb0
bibi <kevindaniel707070@gmail.com>
2026-01-31 14:51:38 -0400

    chore(main): add Jaspect enable annotation

--- src/main/java/com/kdbf/digitalLibrary/DigitalLibraryApplication.java
+++ src/main/java/com/kdbf/digitalLibrary/DigitalLibraryApplication.java
@@ -2,12 +2,14 @@ package com.kdbf.digitalLibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class DigitalLibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DigitalLibraryApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DigitalLibraryApplication.class, args);
+  }
 
 }
