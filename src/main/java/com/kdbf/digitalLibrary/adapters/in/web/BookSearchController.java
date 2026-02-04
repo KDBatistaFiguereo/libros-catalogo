fb22dc351b71d8ef5f41e21e41e9c69b992dc7fd
bibi <kevindaniel707070@gmail.com>
2026-02-04 13:34:09 -0400

    feat(controller): implement save book controller

--- src/main/java/com/kdbf/digitalLibrary/adapters/in/web/BookSearchController.java
+++ src/main/java/com/kdbf/digitalLibrary/adapters/in/web/BookSearchController.java
@@ -5,19 +5,26 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kdbf.digitalLibrary.application.domain.model.entity.Book;
 import com.kdbf.digitalLibrary.application.domain.service.FindBooksByTitleService;
+import com.kdbf.digitalLibrary.application.domain.service.SaveBookService;
 import com.kdbf.digitalLibrary.application.port.in.FindBooksByTitleQuery;
+import com.kdbf.digitalLibrary.application.port.in.SaveBookCommand;
 
 @Controller
 public class BookSearchController {
 
   private final FindBooksByTitleService findBooksService;
+  private final SaveBookService saveBookService;
 
-  public BookSearchController(FindBooksByTitleService findBooksService) {
+  public BookSearchController(FindBooksByTitleService findBooksService, SaveBookService saveBookService) {
     this.findBooksService = findBooksService;
+    this.saveBookService = saveBookService;
   }
 
   @GetMapping("/libros/busqueda")
@@ -28,6 +35,16 @@ public class BookSearchController {
     model.addAttribute("bookList", myBooks);
 
     return "book-search-result";
+
+  }
+
+  @PostMapping("/libros/guardar")
+  public String saveBook(@ModelAttribute SaveBookCommand command,
+      RedirectAttributes redirectAttributes) {
+    saveBookService.saveBook(command);
+    redirectAttributes.addAttribute("title", command.title());
+    redirectAttributes.addFlashAttribute("message", "Book saved succesfully.");
+    return "redirect:/libros/busqueda";
 
   }
 
