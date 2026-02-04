daf970e0afc4bb40eb9c49cba36177e02e9eecb9
bibi <kevindaniel707070@gmail.com>
2026-02-04 13:53:59 -0400

    feat(controller): write controller for persisted books

--- src/main/java/com/kdbf/digitalLibrary/adapters/in/web/BookHistoryController.java
+++ src/main/java/com/kdbf/digitalLibrary/adapters/in/web/BookHistoryController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.kdbf.digitalLibrary.application.domain.model.entity.Book;
+import com.kdbf.digitalLibrary.application.domain.service.GetBooksService;
 import com.kdbf.digitalLibrary.common.session.BookSearchHistory;
 
 import lombok.AllArgsConstructor;
@@ -16,12 +17,20 @@ import lombok.AllArgsConstructor;
 public class BookHistoryController {
 
   private final BookSearchHistory bookSearchHistory;
+  private final GetBooksService getBooksService;
 
   @GetMapping("/libros/historial")
   public String seeHistory(Model model) {
     List<Book> bookHistory = bookSearchHistory.getBookHistory();
     model.addAttribute("historyList", bookHistory);
     return "book-history";
+  }
+
+  @GetMapping("/libros")
+  public String showSavedBooks(Model model) {
+    List<Book> allBooks = getBooksService.getAllBooks();
+    model.addAttribute("savedBooks", allBooks);
+    return "saved-books";
   }
 
 }
